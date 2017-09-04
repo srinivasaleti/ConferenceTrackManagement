4cc595b43b135a8d41c60896ee666e8c04969f3a
srinivasarao <srinivra@thoughtworks.com>
2017-09-04 13:58:18 +0530

    Schedule given talks into tracks of a conference

--- src/com/tw/ConferenceScheduler.java
+++ src/com/tw/ConferenceScheduler.java
@@ -1,12 +1,27 @@
 package com.tw;
 
+import java.util.Iterator;
 import java.util.List;
 
 //Represents a scheduler which schedules given talks into a conference
 class ConferenceScheduler {
 
     void scheduleTalksInto(Conference conference, List<Talk> talks) {
+        while (talks.size() != 0) {
+            Track track = new Track();
+            addTalksToA(track, talks);
+            conference.addTrack(track);
+        }
+    }
 
+    private void addTalksToA(Track track, List<Talk> talks) {
+        for (Iterator<Talk> talkIterator = talks.iterator(); talkIterator.hasNext(); ) {
+            Talk talk = talkIterator.next();
+            if (track.hasDurationOf(talk.duration())) {
+                track.addTalkInASession(talk);
+                talkIterator.remove();
+            }
+        }
     }
 
 }
--- test/com/tw/ConferenceSchedulerTest.java
+++ test/com/tw/ConferenceSchedulerTest.java
@@ -1,5 +1,79 @@
 package com.tw;
 
-public class ConferenceSchedulerTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+class ConferenceSchedulerTest {
+    private ConferenceScheduler conferenceScheduler;
+
+    @BeforeEach
+    void setUp() {
+        this.conferenceScheduler = new ConferenceScheduler();
+    }
+
+    @Test
+    void askSizeOfTalks() {
+        List<Talk> talks = mock(List.class);
+        Conference conference = mock(Conference.class);
+        when(talks.size()).thenReturn(0);
+        conferenceScheduler.scheduleTalksInto(conference, talks);
+
+        verify(talks).size();
+    }
+
+    @Test
+    void askIteratorOnTalks() {
+        List<Talk> talks = mock(List.class);
+        Iterator<Talk> iterator = mock(Iterator.class);
+        Conference conference = mock(Conference.class);
+
+        when(talks.size()).thenReturn(1, 0);
+        when(talks.iterator()).thenReturn(iterator);
+        when(iterator.hasNext()).thenReturn(false);
+
+        conferenceScheduler.scheduleTalksInto(conference, talks);
+
+        verify(talks, times(1)).iterator();
+    }
+
+    @Test
+    void askDurationOfTalkToCheckATrackHasSufficientDuration() {
+        Talk javaTalk = mock(Talk.class);
+        List<Talk> talks = mock(List.class);
+        Iterator<Talk> iterator = mock(Iterator.class);
+        Conference conference = mock(Conference.class);
+
+        when(talks.size()).thenReturn(1, 0);
+        when(talks.iterator()).thenReturn(iterator);
+        when(iterator.hasNext()).thenReturn(true,false);
+        when(iterator.next()).thenReturn(javaTalk);
+        when(javaTalk.duration()).thenReturn(Duration.minute(60));
+
+        conferenceScheduler.scheduleTalksInto(conference, talks);
+
+        verify(javaTalk,times(5)).duration();
+    }
+
+    @Test
+    void addATrackToConference() {
+        List<Talk> talks = mock(List.class);
+        Iterator<Talk> iterator = mock(Iterator.class);
+        Conference conference = mock(Conference.class);
+
+        when(talks.size()).thenReturn(1, 0);
+        when(talks.iterator()).thenReturn(iterator);
+        when(iterator.hasNext()).thenReturn(true,false);
+        when(iterator.next()).thenReturn(new Talk("Java",Duration.minute(90)));
+
+        conferenceScheduler.scheduleTalksInto(conference, talks);
+
+        verify(conference).addTrack(any(Track.class));
+    }
 
 }
+
