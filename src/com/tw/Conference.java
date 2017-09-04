811699c14d6418847dc951dbd0bf8c975ff56ee4
srinivasarao <srinivra@thoughtworks.com>
2017-09-04 13:48:20 +0530

    Add a track to conference

--- src/com/tw/Conference.java
+++ src/com/tw/Conference.java
@@ -5,8 +5,14 @@ import java.util.List;
 //Represents a formal meeting of people with a shared interest,
 class Conference {
 
-    Conference(List<Track> tracksContainer) {
+    private final List<Track> tracksContainer;
 
+    Conference(List<Track> tracksContainer) {
+        this.tracksContainer = tracksContainer;
+    }
+
+    void addTrack(Track track) {
+        tracksContainer.add(track);
     }
 
 }
--- test/com/tw/ConferenceTest.java
+++ test/com/tw/ConferenceTest.java
@@ -1,5 +1,35 @@
 package com.tw;
 
-public class ConferenceTest {
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class ConferenceTest {
+
+    @Test
+    void addATrackToContainer() {
+        List<Track> tracksContainer = mock(List.class);
+        Track track = new Track();
+        Conference conference = new Conference(tracksContainer);
+        conference.addTrack(track);
+
+        verify(tracksContainer).add(track);
+    }
+
+    @Test
+    void addMultipleTracksToContainer() {
+        List<Track> tracksContainer = mock(List.class);
+        Track track = new Track();
+        Track anotherTrack = new Track();
+        Conference conference = new Conference(tracksContainer);
+        conference.addTrack(track);
+        conference.addTrack(anotherTrack);
+
+        verify(tracksContainer).add(track);
+        verify(tracksContainer).add(track);
+    }
 
 }
