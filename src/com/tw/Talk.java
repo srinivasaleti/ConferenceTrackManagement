99d6e688d52678d85543a63ceffe5cddb2cc1859
srinivasarao <srinivra@thoughtworks.com>
2017-09-04 12:57:56 +0530

    Check equality of two times

--- src/com/tw/Talk.java
+++ src/com/tw/Talk.java
@@ -3,15 +3,27 @@ package com.tw;
 //Represents a formal discussions or negotiations over a period
 class Talk {
 
+    private static final String SEPARATOR = " ";
+
     private Duration duration;
+    private Time startTime;
+    private String name;
 
     Talk(String name, Duration duration) {
         this.duration = duration;
+        this.name = name;
     }
 
     Duration duration() {
         return this.duration;
     }
 
+    void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    String representation() {
+        return null;
+    }
 
 }
--- test/com/tw/TimeTest.java
+++ test/com/tw/TimeTest.java
@@ -1,5 +1,43 @@
 package com.tw;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 class TimeTest {
+
+    private Time time;
+
+    @BeforeEach
+    void setUp() {
+        Duration hour = Duration.hour(9);
+        Duration minute = Duration.minute(0);
+        this.time = new Time(hour, minute, "AM");
+    }
+
+    @Test
+    void timeNotEqualToNull() {
+        assertNotEquals(time, null);
+    }
+
+    @Test
+    void timeNotEqualToString() {
+        assertNotEquals(time, "Time");
+    }
+
+    @Test
+    void timeEqualToItSelf() {
+        assertEquals(time, time);
+    }
+
+    @Test
+    void timeEqualsToSameTime() {
+        Duration minute = Duration.minute(0);
+        Duration hour = Duration.hour(9);
+        Time sameTime = new Time(hour, minute, "AM");
+        assertEquals(time, sameTime);
+    }
 
 }
