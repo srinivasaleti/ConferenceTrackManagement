a0814d13eeb82c65049d111d9bd76932aaec2fde
srinivasarao <srinivra@thoughtworks.com>
2017-09-04 12:04:28 +0530

    Get duration of a talk

--- src/com/tw/Talk.java
+++ src/com/tw/Talk.java
@@ -3,7 +3,15 @@ package com.tw;
 //Represents a formal discussions or negotiations over a period
 class Talk {
 
+    private Duration duration;
+
     Talk(String name, Duration duration) {
+        this.duration = duration;
     }
+
+    Duration duration() {
+        return this.duration;
+    }
+
 
 }
--- test/com/tw/TalkTest.java
+++ test/com/tw/TalkTest.java
@@ -1,5 +1,28 @@
 package com.tw;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class TalkTest {
+
+    @Test
+    void expectedDurationOfPythonTalk() {
+        Duration duration = Duration.minute(60);
+        String python = "Python";
+        Talk pythonTalk = new Talk(python, duration);
+
+        assertEquals(duration, pythonTalk.duration());
+    }
+
+    @Test
+    void expectedDurationOfJavaTalk() {
+        Duration duration = Duration.lightning(2);
+        String java = "Java";
+        Talk javaTalk = new Talk(java, duration);
+        Duration durationInMinute = Duration.minute(10);
+
+        assertEquals(durationInMinute, javaTalk.duration());
+    }
 
 }
