925886833f1db7b9ae552f4ef8d058df51ea4f3c
srinivasarao <srinivra@thoughtworks.com>
2017-09-04 11:29:17 +0530

    Verify equality of two durations

--- src/com/tw/Duration.java
+++ src/com/tw/Duration.java
@@ -3,4 +3,29 @@ package com.tw;
 //Represents the time during which something continues
 class Duration {
 
+    private int value;
+    private int baseValue;
+    private String representation;
+
+    Duration(int value, int conversionFactor, String representation) {
+        this.value = value;
+        this.baseValue = this.value * conversionFactor;
+        this.representation = representation;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+        if (this.getClass() != object.getClass()) {
+            return false;
+        }
+        if (this == object) {
+            return true;
+        }
+        Duration that = (Duration) object;
+        return this.baseValue == that.baseValue;
+    }
+
 }
--- test/com/tw/DurationTest.java
+++ test/com/tw/DurationTest.java
@@ -1,4 +1,45 @@
 package com.tw;
 
-public class DurationTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class DurationTest {
+
+    private Duration duration;
+    private int value = 1;
+    private int conversionFactor = 1;
+    private String minute = "Minute";
+
+    @BeforeEach
+    void setUp() {
+        duration = new Duration(value, conversionFactor, minute);
+    }
+
+    @Test
+    void durationNotEqualToNull() {
+        assertNotEquals(duration, null);
+    }
+
+    @Test
+    void durationNotEqualToString() {
+        String durationString = "Duration";
+
+        assertNotEquals(duration, durationString);
+    }
+
+    @Test
+    void durationEqualToItSelf() {
+        assertEquals(duration, duration);
+    }
+
+    @Test
+    void durationEqualToSameDuration() {
+        Duration sameDuration = new Duration(value, conversionFactor, minute);
+
+        assertEquals(duration, sameDuration);
+    }
+
 }
