78fd4e5fe1337a33f41568bd2a36c943595faaab
srinivasarao <srinivra@thoughtworks.com>
2017-09-04 13:38:43 +0530

    Add a talk to a session in a track

--- src/com/tw/Track.java
+++ src/com/tw/Track.java
@@ -1,4 +1,40 @@
 package com.tw;
 
+import java.util.ArrayList;
+
 class Track {
+
+    private static final Time MORNING_SESSION_STARTING_TIME = new Time(Duration.hour(9), Duration.minute(0), "AM");
+    private static final Time LUNCH_SESSION_STARTING_TIME = new Time(Duration.hour(12), Duration.minute(0), "PM");
+    private static final Time AFTERNOON_SESSION_STARTING_TIME = new Time(Duration.hour(1), Duration.minute(0), "PM");
+    private static final Duration MORNING_SESSION_DURATION = Duration.minute(180);
+    private static final Duration LUNCH_SESSION_DURATION = Duration.minute(60);
+    private static final Duration AFTERNOON_SESSION_DURATION = Duration.minute(240);
+    private static final Talk LUNCH_TALK = new Talk("Lunch", Duration.minute(60));
+
+    private Session morningSession;
+    private Session lunchSession;
+    private Session afterNoonSession;
+
+    Track() {
+        this.morningSession = new Session(new ArrayList<>(), MORNING_SESSION_STARTING_TIME, MORNING_SESSION_DURATION);
+        this.afterNoonSession = new Session(new ArrayList<>(), AFTERNOON_SESSION_STARTING_TIME, AFTERNOON_SESSION_DURATION);
+        this.lunchSession = new Session(new ArrayList<>(), LUNCH_SESSION_STARTING_TIME, LUNCH_SESSION_DURATION);
+        this.lunchSession.addATalk(LUNCH_TALK);
+    }
+
+    void addTalkInASession(Talk talk) {
+        if (this.morningSession.hasDurationOf(talk.duration())) {
+            this.morningSession.addATalk(talk);
+            return;
+        }
+        this.afterNoonSession.addATalk(talk);
+    }
+
+    public String representation() {
+        return this.morningSession.representation()
+                + this.lunchSession.representation()
+                + this.afterNoonSession.representation();
+    }
+
 }
--- test/com/tw/TrackTest.java
+++ test/com/tw/TrackTest.java
@@ -1,5 +1,35 @@
 package com.tw;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class TrackTest {
+
+    @Test
+    void addTalkToASession() {
+        Talk talk = new Talk("Java", Duration.minute(60));
+        Track track = new Track();
+        String expected = "9:00 AM Java 60 Minutes" + "\n12:00 PM Lunch 60 Minutes\n";
+
+        track.addTalkInASession(talk);
+
+        assertEquals(expected, track.representation());
+    }
+
+    @Test
+    void addMultipleTalksToTrack() {
+        Talk javaTalk = new Talk("Java", Duration.minute(180));
+        Talk pythonTalk = new Talk("Python", Duration.lightning(5));
+        Track track = new Track();
+        String expected = "9:00 AM Java 180 Minutes\n" +
+                "12:00 PM Lunch 60 Minutes\n" +
+                "1:00 PM Python 5 Lightnings\n";
+
+        track.addTalkInASession(javaTalk);
+        track.addTalkInASession(pythonTalk);
+
+        assertEquals(expected, track.representation());
+    }
 
 }
